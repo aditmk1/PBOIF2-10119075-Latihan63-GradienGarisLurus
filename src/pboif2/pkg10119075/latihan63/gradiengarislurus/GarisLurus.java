a10ccf11b6f4fad5f00705f9d2ef5589f2b93bae
aditmk1 <aditiyamk1010@gmail.com>
2020-11-29 10:42:19 +0700

    running

---
+++ src/pboif2/pkg10119075/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119075.latihan63.gradiengarislurus;
+
+/**
+Nama : Aditiya musthafa kamil
+Kelas : PBOIF2
+NIM : 10119075
+Deskripsi Program : Program ini berisi program untuk menampilkan garis lurus
+ */
+public interface GarisLurus {
+    public int hitungGradien();
+}
